d21fdec4885c1523d067613aafb9fec874202cf0
Martin J. Evans <bohica@ntlworld.com>
2011-01-27 10:47:07 +0000

    Bumped to 1.28_3 Fixed MANIFEST which named 2 files incorrectly. Fixed use of PREREQ_PRINT in versions of ExtUtils::MakeMaker which   don't support it. Added Perl and ExtUtils::MakeMaker version output to build process.

--- ODBC.pm
+++ ODBC.pm
@@ -19,7 +19,7 @@ require 5.006;
 # see discussion on dbi-users at
 # http://www.nntp.perl.org/group/perl.dbi.dev/2010/07/msg6096.html and
 # http://www.dagolden.com/index.php/369/version-numbers-should-be-boring/
-$DBD::ODBC::VERSION = '1.28_2';
+$DBD::ODBC::VERSION = '1.28_3';
 
 {
     ## no critic (ProhibitMagicNumbers ProhibitExplicitISA)
@@ -524,7 +524,7 @@ DBD::ODBC - ODBC Driver for DBI
 
 =head1 VERSION
 
-This documentation refers to DBD::ODBC version 1.28_2.
+This documentation refers to DBD::ODBC version 1.28_3.
 
 =head1 SYNOPSIS
 
