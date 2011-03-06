fa44290b1ecf566e50f933e260a3e8659a0aacfb
Martin J. Evans <bohica@ntlworld.com>
2011-03-06 17:11:57 +0000

    Change back to 1.28_5

--- ODBC.pm
+++ ODBC.pm
@@ -19,7 +19,7 @@ require 5.006;
 # see discussion on dbi-users at
 # http://www.nntp.perl.org/group/perl.dbi.dev/2010/07/msg6096.html and
 # http://www.dagolden.com/index.php/369/version-numbers-should-be-boring/
-$DBD::ODBC::VERSION = '1.29';
+$DBD::ODBC::VERSION = '1.28_5';
 
 {
     ## no critic (ProhibitMagicNumbers ProhibitExplicitISA)
@@ -562,7 +562,7 @@ DBD::ODBC - ODBC Driver for DBI
 
 =head1 VERSION
 
-This documentation refers to DBD::ODBC version 1.29.
+This documentation refers to DBD::ODBC version 1.28_5.
 
 =head1 SYNOPSIS
 
