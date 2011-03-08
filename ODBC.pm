701875ffa9b612874e222ff66ffbf924bc89d2be
Martin J. Evans <bohica@ntlworld.com>
2011-03-08 11:21:47 +0000

    Bump to 1.29 for real release

--- ODBC.pm
+++ ODBC.pm
@@ -19,7 +19,7 @@ require 5.006;
 # see discussion on dbi-users at
 # http://www.nntp.perl.org/group/perl.dbi.dev/2010/07/msg6096.html and
 # http://www.dagolden.com/index.php/369/version-numbers-should-be-boring/
-$DBD::ODBC::VERSION = '1.28_5';
+$DBD::ODBC::VERSION = '1.29';
 
 {
     ## no critic (ProhibitMagicNumbers ProhibitExplicitISA)
@@ -562,7 +562,7 @@ DBD::ODBC - ODBC Driver for DBI
 
 =head1 VERSION
 
-This documentation refers to DBD::ODBC version 1.28_5.
+This documentation refers to DBD::ODBC version 1.29.
 
 =head1 SYNOPSIS
 
@@ -1443,7 +1443,7 @@ believes to be a placeholder in a comment e.g.,
 I cannot be exact about support for ignoring placeholders in literals
 but it has existed for a long time in DBD::ODBC. Support for ignoring
 placeholders in comments was added in 1.24_2. If you find a case where
-a named placeholder is not ignored and should be see
+a named placeholder is not ignored and should be, see
 L</odbc_ignore_named_placeholders> for a workaround and mail me an
 example along with your ODBC driver name.
 
