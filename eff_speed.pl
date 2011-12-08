c66f0e1c353a4451c5c48815b4c49935643dc9ee
Martin J. Evans <bohica@ntlworld.com>
2011-12-08 08:27:14 +0000

    changes to speed test

--- eff_speed.pl
+++ eff_speed.pl
@@ -2,34 +2,52 @@ use DBI;
 use Data::Dumper;
 use strict;
 use warnings;
-
-my $h = DBI->connect("dbi:ODBC:DSN=baugi","sa","easysoft",
-		     {RaiseError => 1, PrintError => 0,odbc_disable_array_operations => 0});
-eval {
-    local $h->{PrintError} = 0;
-    $h->do(q/drop table two/);
-};
-
-$h->do(q/create table two (a varchar(20))/);
+use Benchmark;
 
 my $fetch_row = 0;
-
-my $x = '1' x 2000;
-
+my $x = '1' x 1000;
 my @p = split (//,$x);
 
-for (my $i = 0; $i < 100; $i++) {
-    doit();
+timethese(20, {
+    'execute_for_fetch' => sub {one()},
+    'dbi' => sub {two()},
+	  });
+
+sub dbconnect {
+    my $disable = shift;
+
+    my $h =  DBI->connect("dbi:ODBC:DSN=baugi","sa","easysoft",
+			  {RaiseError => 1, PrintError => 0,
+			   odbc_disable_array_operations => $disable,
+			   odbc_batch_size => 50});
+    eval {
+	local $h->{PrintError} = 0;
+	$h->do(q/drop table two/);
+    };
+
+    $h->do(q/create table two (a varchar(20))/);
+    return $h;
 }
 
-$h->disconnect;
+sub one {
+    my $h = dbconnect(0);
+    doit($h);
+    $h->disconnect;
+}
+
+sub two {
+    my $h = dbconnect(1);
+    doit($h);
+    $h->disconnect;
+}
 
 sub doit {
-    print "dbh odbc_batch_size=", $h->{odbc_batch_size}, "\n";
+    my $h = shift;
+    #print "dbh odbc_batch_size=", $h->{odbc_batch_size}, "\n";
     my $s = $h->prepare(q/insert into two values(?)/);
-    print "sth odbc_batch_size=", $s->{odbc_batch_size}, "\n";
+    #print "sth odbc_batch_size=", $s->{odbc_batch_size}, "\n";
     my ($tuples, $rows, @tuple_status);
-    print "About to run execute_for_fetch\n";
+    #print "About to run execute_for_fetch\n";
     eval {
         ($tuples, $rows) = $s->execute_for_fetch(\&fetch_sub, \@tuple_status);
     };
@@ -52,7 +70,7 @@ sub doit {
 sub fetch_sub {
     #print "fetch_sub $fetch_row\n";
     if ($fetch_row == @p) {
-        print "returning undef\n";
+        #print "returning undef\n";
         $fetch_row = 0;
         return;
     }
