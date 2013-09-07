2fce201e6f5a75c0a3c1e4d5b448adb767e50ec9
Martin J. Evans <github@virginmedia.com>
2013-09-07 09:49:45 +0100

    fix errors in log_read.pl example and expand
    TYPE => 999 was a test that should not have been checked in.
    Expand to show the difference between retrieving as SQL_BINARY and
    SQL_CHAR.

--- examples/lob_read.pl
+++ examples/lob_read.pl
@@ -5,26 +5,40 @@
 #use Log::Log4perl qw(:easy);
 #Log::Log4perl->easy_init($DEBUG);
 #use DBIx::Log4perl;
-use DBI;
+use DBI qw(:sql_types);
 use strict;
 use warnings;
 
-#$h = DBIx::Log4perl->connect("dbi:ODBC:baugi","sa","easysoft". {PrintError => 1});
-my $h = DBI->connect("dbi:ODBC:baugi","sa","easysoft",
-                     {PrintError => 1, RaiseError => 1, PrintWarn => 1});
-my $s = $h->prepare(q{select 'frederick'});
+#my $h = DBI->connect("dbi:ODBC:baugi","sa","easysoft",
+#                     {PrintError => 1, RaiseError => 1, PrintWarn => 1});
+my $h = DBI->connect;
+$h->{PrintError} = $h->{RaiseError} = $h->{PrintWarn} = 1;
+
+my $s = $h->prepare(q{select 'frederickfrederick'});
 $s->execute;
 $s->bind_col(1, undef, {TreatAsLOB=>1});
 $s->fetch;
-# SQL_SUCCESS = 0
-# SQL_SUCCESS_WITH_INFO = 1
-# SQL_NO_DATA = 100
-my $len;
-$s->{RaiseError} = 0;
-while($len = $s->odbc_lob_read(1, \my $x, 8, {Type => 999})) {
-    print "len=$len, x=$x\n";
+
+getit($s, SQL_BINARY);
+
+$s = $h->prepare(q{select 'frederickfrederick'});
+$s->execute;
+$s->bind_col(1, undef, {TreatAsLOB=>1});
+$s->fetch;
+
+# NOTE the difference between receiving something as binary and as a char
+# ODBC's SQLGetData is defined as putting a terminating NUL chr at the
+# end of strings so even though we ask for 8 we get 7 bytes
+getit($s, SQL_CHAR);
+
+sub getit{
+    my ($s, $type) = @_;
+    my $len;
+    while($len = $s->odbc_lob_read(1, \my $x, 8, {TYPE => $type})) {
+        print "len=$len, x=$x, ", length($x), "\n";
+    }
+    print "len at end = $len\n";
+    my $x;
+    $len = $s->odbc_lob_read(1, \$x, 8);
+    print  "len after read $len\n";
 }
-print "len at end = $len\n";
-my $x;
-$len = $s->odbc_lob_read(1, \$x, 8);
-$len = $s->odbc_lob_read(1, \$x, 8);
